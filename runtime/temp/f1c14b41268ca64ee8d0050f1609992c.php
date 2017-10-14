3e38a3e76d9e20fd7f5ec498cf85382b481112e3
ihome520 <31379279+ihome520@users.noreply.github.com>
2017-10-14 21:05:58 +0800

    tp++

---
+++ runtime/temp/f1c14b41268ca64ee8d0050f1609992c.php
@@ -0,0 +1,45 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:72:"D:\phpStudy\WWW\tp5sst\public/../application/index\view\index\index.html";i:1507984762;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>html5 模板</title>
+    <meta name="Keywords" content=""/>
+    <meta name="Description" content=""/>
+    <!-- 内核解释器 当IE9以下时，可能要用到 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- 以下这段是移动端优先 页面缩放比例为1 -->
+    <meta name="viewport" content="width-device-width,initial-scale=1,maximum-scale=1,user-scalabel=no">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+    <!-- 引入3个主要文件，依次引入bs的css 然后优先引入jq（bs基于这个，要先加载）再引入bs的js -->
+    <link rel="stylesheet" href="__PUBLIC__/Bootstrap/css/bootstrap.min.css">
+    <script type="text/javascript" src="__PUBLIC__/Js/jquery-2.0.3.min.js"></script>
+    <script type="text/javascript" src="__PUBLIC__/Bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="__PUBLIC__/layui/layui.js"></script>
+    <script type="text/javascript" src="__PUBLIC__/layui/css/layui.css"></script>
+    <!-- 表单验证vd的js -->
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="style.css"> -->
+    <!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    html 5 and thinkphp 5.0
+    <div class="header">
+        头部
+    </div>
+    <div class="container">
+        内容盒子
+        <div class="top">
+            顶部
+        </div>
+        <div class="center">
+            中部
+        </div>
+        <div class="footer">
+            底部
+        </div>
+    </div>
+</body>
+</html>
